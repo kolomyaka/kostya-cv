dade089c7094a1598d5c665d390be90f8f7d14b9
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2024-11-26 18:36:59 +0300

    Accordion main page

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const accordionElements = document.getElementsByClassName('accordion__title')
+
+for (let i = 0; i < accordionElements.length; i++) {
+    const currentAccordion = accordionElements[i]
+    currentAccordion.addEventListener('click', () => {
+      const arrow = currentAccordion.querySelector('.accordion__title--arrow');
+      const content = currentAccordion.nextElementSibling
+      arrow.classList.toggle('active');
+      content.classList.toggle('active');
+  })
+}
