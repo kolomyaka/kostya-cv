fedab4b5d5525bf13c4eb8ebd5b71cf2ad0bb6c6
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2024-12-01 13:42:27 +0300

    add scroll top button

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 const accordionElements = document.getElementsByClassName('accordion__title')
+const scrollButton = document.querySelector('.scroll-button')
 
 for (let i = 0; i < accordionElements.length; i++) {
     const currentAccordion = accordionElements[i]
@@ -11,3 +12,25 @@ for (let i = 0; i < accordionElements.length; i++) {
       content.classList.toggle('active');
   })
 }
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+function toggleScrollButton() {
+    console.log('here', window.scrollY)
+    if (window.scrollY > 200) {
+        scrollButton.classList.add('show')
+    } else {
+        scrollButton.classList.remove('show')
+    }
+}
+
+// Add event listener to the scroll button
+scrollButton.addEventListener('click', scrollToTop)
+
+// Add event listener to window scroll
+window.addEventListener('scroll', toggleScrollButton)
+
+// Initial check to see if the button should be visible
+toggleScrollButton()
