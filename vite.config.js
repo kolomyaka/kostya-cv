917b70eb07f92b394b7128034cd2a6830e1f5805
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2025-08-29 18:46:28 +0300

    Change admortgage to it contact

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,9 @@ export default defineConfig(() => {
                 input: {
                     main: resolve(__dirname, 'index.html'),
                     about: resolve(__dirname, 'about.html'),
-                    "ad-mortgage": resolve(__dirname, 'ad-mortgage.html'),
+                    "it-contact": resolve(__dirname, 'it-contact.html'),
                     rosbank: resolve(__dirname, 'rosbank.html'),
                     vtb: resolve(__dirname, 'vtb.html'),
-                    "yami-yami": resolve(__dirname, 'yami-yami.html'),
                     "health": resolve(__dirname, 'health.html'),
                     yandex_9e20bf8c2d6c26fe: resolve(__dirname, 'yandex_9e20bf8c2d6c26fe.html')
                 }
