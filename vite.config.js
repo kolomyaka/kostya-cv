5780b8e8d7510195b543d1e1fdb2e64b1106492a
Nikita <66018338+kolomyaka1@users.noreply.github.com>
2025-10-15 12:34:20 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig(() => {
                     rosbank: resolve(__dirname, 'rosbank.html'),
                     vtb: resolve(__dirname, 'vtb.html'),
                     "health": resolve(__dirname, 'health.html'),
-                    "it-contact": resolve(__dirname, 'index.html'),
+                    "it-contact": resolve(__dirname, 'it-contact.html'),
                     yandex_9e20bf8c2d6c26fe: resolve(__dirname, 'yandex_9e20bf8c2d6c26fe.html')
                 }
             }
